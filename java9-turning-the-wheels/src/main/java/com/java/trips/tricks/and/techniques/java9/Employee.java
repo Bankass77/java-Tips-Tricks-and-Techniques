5a488af0ac4c4850b1847ce9eebdce3a52939cde
aguindo <a.guindo@groupeonepoint.com>
2024-07-24 15:22:54 +0200

    Collections Api update

---
+++ java9-turning-the-wheels/src/main/java/com/java/trips/tricks/and/techniques/java9/Employee.java
@@ -0,0 +1,15 @@
+package com.java.trips.tricks.and.techniques.java9;
+
+import lombok.*;
+
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Employee {
+
+    private int empId;
+
+    private String name;
+}
