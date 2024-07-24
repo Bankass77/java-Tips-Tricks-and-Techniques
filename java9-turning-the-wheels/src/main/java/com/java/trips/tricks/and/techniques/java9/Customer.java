62d0f5b7bc4b01212db009e935d791c9306f5c21
aguindo <a.guindo@groupeonepoint.com>
2024-07-24 11:08:21 +0200

    dropWhile use case

---
+++ java9-turning-the-wheels/src/main/java/com/java/trips/tricks/and/techniques/java9/Customer.java
@@ -0,0 +1,25 @@
+package com.java.trips.tricks.and.techniques.java9;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class Customer implements Comparable<Customer> {
+
+    private int customerId;
+
+    private String customerName;
+
+    private double customerBalance;
+
+    @Override
+    public int compareTo(Customer customer) {
+
+        //Puis que je souhaite comparer les soldes des clients, donc on fait:
+        return Double.compare(this.customerBalance, customer.customerBalance);
+    }
+
+}
