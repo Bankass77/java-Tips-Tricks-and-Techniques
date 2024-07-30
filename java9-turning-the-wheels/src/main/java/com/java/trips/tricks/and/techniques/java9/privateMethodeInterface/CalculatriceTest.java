87fea698b8db21cbdfcf27c82a1487031fc92a06
aguindo <a.guindo@groupeonepoint.com>
2024-07-30 11:56:57 +0200

    Ajout de methode private dans une interface

---
+++ java9-turning-the-wheels/src/main/java/com/java/trips/tricks/and/techniques/java9/privateMethodeInterface/CalculatriceTest.java
@@ -0,0 +1,43 @@
+package src.main.java.com.java.trips.tricks.and.techniques.java9.privateMethodeInterface;
+
+import org.junit.Assert;
+import org.junit.Test;
+import src.main.java.com.java.trips.tricks.and.techniques.java9.privateMethodeInterface.util.CalculatriceUtils;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatriceTest {
+
+    Calculatrice calculatrice = new Calculatrice() {
+
+    };
+
+    @Test
+    public void testAddition() {
+        assertEquals(5, calculatrice.addition(2, 3));
+        assertEquals(0, calculatrice.addition(-1, 1));
+        assertEquals(-4, calculatrice.addition(-2, -2));
+    }
+
+    @Test
+    public void testSoustraction() {
+        assertEquals(1, calculatrice.subtraction(3, 2));
+        assertEquals(-2, calculatrice.subtraction(-1, 1));
+        assertEquals(0, calculatrice.subtraction(-2, -2));
+    }
+
+    @Test
+    public void testMultiplication() {
+        assertEquals(6, calculatrice.multiplication(2, 3));
+        assertEquals(-1, calculatrice.multiplication(-1, 1));
+        assertEquals(4, calculatrice.multiplication(-2, -2));
+    }
+
+    //ou avec l'utilisation de la classe utilitaire pour tester la méthode private
+    @Test
+    public void testOperationCommune() {
+        assertEquals(5, CalculatriceUtils.operationCommune(2, 3));
+        assertEquals(0, CalculatriceUtils.operationCommune(-1, 1));
+        assertEquals(-4, CalculatriceUtils.operationCommune(-2, -2));
+    }
+}
---
+++ java9-turning-the-wheels/src/main/java/com/java/trips/tricks/and/techniques/java9/privateMethodeInterface/util/CalculatriceUtils.java
@@ -0,0 +1,9 @@
+package src.main.java.com.java.trips.tricks.and.techniques.java9.privateMethodeInterface.util;
+
+public class CalculatriceUtils {
+
+    public static int operationCommune(int a, int b) {
+        return a + b;
+    }
+
+}
